f0e32fc32add2b7dc9c0f9f2a8219ffe1ed59f37
testroyer <alperozer2007@gmail.com>
2023-05-11 19:55:36 +0300

    get_x_inputs

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #initial commit
 
-from musicmaker.Tone import Tone
+def get_x_inputs(starting_point:int , ending_point:int , interval:float):
+    result_array = [starting_point]
+    current_point = starting_point
+    while current_point < ending_point:
+        if (current_point + interval) > ending_point:
+            return "Error"
+        current_point += interval
+        result_array.append(current_point)
+
+    return result_array
+        
+
+print(get_x_inputs(10 , 15 , 0.5)) 
