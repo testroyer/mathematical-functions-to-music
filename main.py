f31f42d9ba2be4d2f7327a0f34adea4775d0124a
testroyer <alperozer2007@gmail.com>
2023-05-12 18:26:15 +0300

    did something

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-#initial commit
+import musicmaker
+
+BPM = 150
+beat = 60 / 150
 
 def get_x_inputs(starting_point:int , ending_point:int , interval:float):
     result_array = [starting_point]
@@ -10,6 +13,21 @@ def get_x_inputs(starting_point:int , ending_point:int , interval:float):
         result_array.append(current_point)
 
     return result_array
-        
 
-print(get_x_inputs(10 , 15 , 0.5)) 
+def get_mathemathic_expression(variable_input):
+    mathemathic_expression = lambda x : x**3
+    return mathemathic_expression(variable_input)
+
+def mathemathic_function(inputs):
+    return_array = []
+    for input in inputs:
+        return_array.append(get_mathemathic_expression(input))
+
+    return return_array
+
+def turn_values_into_frequencies():
+    lowest_note = 82.41
+
+
+
+print(mathemathic_function(get_x_inputs(-10 , 10 , 1)))
